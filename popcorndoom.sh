9112ec68f44a49d11ad496a5d6a84836a0b1d3f7
Bronzu <46759892+Bronzu@users.noreply.github.com>
2020-03-18 11:25:15 +0000

    Launching with defaults works

--- popcorndoom.sh
+++ popcorndoom.sh
@@ -12,14 +12,14 @@ if [[ -z $(pacman -Q | grep crispy-doom) ]]; then
 fi
 
 #To configure the script edit these values:
-WADpath=~/Gry/PC/Doom/WADs
-MODpath=~/Gry/PC/Doom/Mods
-SAVEpath=~/Gry/PC/Doom/Saves
+WADpath="/home/kacper/Gry/PC/Doom/WADs"
+MODpath="~/Gry/PC/Doom/Mods"
+SAVEpath="~/Gry/PC/Doom/Saves"
 
 #These are default values to launch with
 #and will be overwritten if you choose
 #to enable persistency of last used arguments
-WAD=doom.wad
+WAD="doom.wad"
 MOD=
 DIFFICULTY=3
 FAST=no
@@ -32,17 +32,49 @@ SOUNDeffects=yes
 #of last used arguments, it is also advised
 #to set a folder for the persistency file
 
-#These are program values, plase do not edit
-#them if you don't know what they do
-CHOICE=
+### PROGRAM DATA ###
+DIALOG_CANCEL=1
+DIALOG_ESC=255
+BACK_TITLE="--backtitle PopcorndDoom"
 
-dialog  --backtitle "PopcornDoom" \
-	--menu "Welcome to PopcornDoom\nthe crispydoom launcher!" 15 30 7 \
-	1 "Game WAD" \
-	2 "Mod WAD" \
-	3 "Additional options" \
-	4 "Difficulty" \
-	5 "Level" \
-	6 "Launch crispydoom" \
-	7 "PopcornDoom version"
+while true; do
+	exec 3>&1
+	choice=$(dialog  --backtitle "PopcornDoom" \
+			--title "Main menu" \
+			--cancel-label "Exit" \
+			--menu "Welcome to PopcornDoom\nthe crispydoom launcher!" 15 30 7 \
+			1 "Game WAD" \
+			2 "Mod WAD" \
+			3 "Additional options" \
+			4 "Difficulty" \
+			5 "Level" \
+			6 "Credits" \
+			7 "Launch crispydoom" 2>&1 1>&3)
+	exit_status=$?
+	exec 3>&-
+	case $exit_status in
+		$DIALOG_CANCEL)
+		clear
+		exit
+		;;
+		$DIALOG_ESC)
+		clear
+		exit 1
+		;;
+	esac
 
+	case $choice in
+		0)
+		clear
+		echo "Program terminated."
+		exit
+		;;
+		1)
+		;;
+		7)
+		clear
+		$(crispy-doom -iwad $WADpath/$WAD)
+		exit
+		;;
+	esac
+done
